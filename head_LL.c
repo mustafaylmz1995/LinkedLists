70ef8bb2cb2286cf7a365921c329ef11067b4ba0
Mustafa YILMAZ <mustafaylmz1995@gmail.com>
2021-01-16 17:53:31 +0300

    Update head_LL.c

--- head_LL.c
+++ head_LL.c
@@ -1,128 +1,129 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 
-typedef struct NODE{
-    int data;
-    struct NODE *next;
+typedef struct NODE {
+	int data;
+	struct NODE* next;
 } node;
 
-typedef struct header{
+typedef struct header {
 	int mininfo;
 	int maxinfo;
 	int ncount;
-	struct NODE *next;
+	struct NODE* next;
 }headerNode;
 
 
-headerNode *deletestart(headerNode *start){
+headerNode* deletestart(headerNode* start) {
 	//start next değerini tutacak bir ptr olustur
-	node *ptr;
+	node* ptr;
 	ptr = start->next;
-	
+
 	//ptr pointer'ı silinmeden önce head node'un degerlerinde 
 	//bir degisikliğe sebep olup olmadığına bakıyoruz.
 	start->ncount = start->ncount - 1;
-	
-	if(start->maxinfo == ptr->data){
-		node *temp;
+
+	if (start->maxinfo == ptr->data) {
+		node* temp;
 		temp = start->next->next;
-		
+
 		start->maxinfo = temp->data;
 		//max info güncelle
-		while(temp->next != NULL){
-			if(start->maxinfo < temp->data)
+		while (temp->next != NULL) {
+			if (start->maxinfo < temp->data)
 				start->maxinfo = temp->data;
+			temp = temp->next;
 		}
 	}
-	
-	if(start->mininfo == ptr->data){
-		node *temp;
+
+	if (start->mininfo == ptr->data) {
+		node* temp;
 		temp = start->next->next;
-		
+
 		start->mininfo = temp->data;
 		//min info güncelle
-		while(temp->next != NULL){
-			if(start->mininfo < temp->data)
+		while (temp->next != NULL) {
+			if (start->mininfo > temp->data)
 				start->mininfo = temp->data;
+			temp = temp->next;
 		}
 	}
-	
+
 	//start'ın next degeri su anki ptr'den sonraki deger olmalıdır.
 	start->next = ptr->next;
 	//ptr next bağlantısını koparıp free ediyoruz.
 	ptr->next = NULL;
 	free(ptr);
-	
+
 	return start;
 }
 
 
-headerNode *insertstart(headerNode *start, int insert_this){
-    //yeni bir node olustur ve degeri ata
-	node *newNode;
-	newNode = (node *)malloc(sizeof(node));	
+headerNode* insertstart(headerNode* start, int insert_this) {
+	//yeni bir node olustur ve degeri ata
+	node* newNode;
+	newNode = (node*)malloc(sizeof(node));
 	newNode->data = insert_this;
-	
+
 	//yeni node, head node'u olacağı için şu anki start node'unun next
 	//değerini yeni node'un next değeri olarak ayarla
 	newNode->next = start->next;
 	//Start'ın next pointer'ının yeni node'u
 	//göstermesini sağla
 	start->next = newNode;
-	
+
 	//start node'unun değerlerini güncelle
 	start->ncount = start->ncount + 1;
-	
-	if(start->maxinfo < insert_this)
+
+	if (start->maxinfo < insert_this)
 		start->maxinfo = insert_this;
-	
-	if(start->mininfo > insert_this)
+
+	if (start->mininfo > insert_this)
 		start->mininfo = insert_this;
 
-    return start;
+	return start;
 }
 
-headerNode *create_hll(headerNode *start){
-	
+headerNode* create_hll(headerNode* start) {
+
 	start->next = NULL;
 	start->ncount = 0;
 	start->maxinfo = 0;
 	start->mininfo = 0;
-	
-	for(int i=1; i<5; i++){
-		start = insertstart(start, i);
-	}
-	
-	for(int i=-1; i>-5; i--){
+
+	for (int i = 1; i < 5; i++) {
 		start = insertstart(start, i);
 	}
 
-    return start;
+	for (int i = -1; i > -5; i--) {
+		start = insertstart(start, i);
+	}
+
+	return start;
 }
 
-headerNode *traverse_hll(headerNode *start){
-	node *ptr;
+headerNode* traverse_hll(headerNode* start) {
+	node* ptr;
 	int i = 0;
 	//start node'unun sonraki node'unu göster
 	ptr = start->next;
-	
+
 	//node'ların next değerleri null olana dek ilerle	
-	while(ptr->next != NULL){
+	while (ptr->next != NULL) {
 		printf("%d\t:\t%d\n", i, ptr->data);
 		ptr = ptr->next;
-		i = i+1;
+		i = i + 1;
 	}
 	//son node'u da yazdır
 	printf("%d\t:\t%d\n", i, ptr->data);
-	
+
 	return start;
 
 }
 
-headerNode *giveInfo(headerNode *start){
-	
+headerNode* giveInfo(headerNode* start) {
+
 	printf("\n___________________________\n");
 	printf("Total Number of Node Counts : %d\n", start->ncount);
 	printf("Max Value of the nodes : %d\n", start->maxinfo);
@@ -133,25 +134,26 @@ headerNode *giveInfo(headerNode *start){
 }
 
 
-int main(void){
-    
-	headerNode hNode, *start;
+int main(void) {
+
+	headerNode hNode, * start;
 	start = &hNode;
 	start = create_hll(start);
 	start = insertstart(start, -5555);
-	
-    //traversing
+
+	//traversing
 	start = traverse_hll(start);
 	//give info
 	start = giveInfo(start);
-	
+
 	start = deletestart(start);
-	
+
 	printf("\nAfter delete process!\n");
 	//traversing
 	start = traverse_hll(start);
 	//give info
 	start = giveInfo(start);
 
-    return 0;
+	return 0;
+};
 }
